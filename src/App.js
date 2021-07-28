c88ee12a132a91fdf2aed6ea14f89725fdb074da
Zartab Nakhwa <nzartab@gmail.com>
2021-07-28 09:46:48 +0530

    Level-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button,ButtonGroup } from '@material-ui/core';
+import { Button, ButtonGroup } from '@material-ui/core';
 
-import { Save,Delete} from '@material-ui/icons';
+import { Save, Delete } from '@material-ui/icons';
 import ButtonComponent from './components/ButtonComponent';
 import CheckBoxComponent from './components/CheckBoxComponent';
 import TextFieldComponent from './components/TextFieldComponent';
 import StyledButtonComponent from './components/StyledButtonComponent';
 
-import { ThemeProvider,createTheme } from '@material-ui/core/styles';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
-import { Container } from '@material-ui/core';
+import { Container, Paper, Grid } from '@material-ui/core';
 import '@fontsource/roboto';
 
-const theme=createTheme({
-  palette:{
-    primary:{
-      main:'#2884f0'
+import { AppBar,Toolbar,IconButton } from '@material-ui/core';
+import { Menu } from '@material-ui/icons';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2884f0'
     },
-    secondary:{
-      main:'#40d50a'
+    secondary: {
+      main: '#40d50a'
+    },
+    white: {
+      main: '#fff'
     }
   }
 })
@@ -27,24 +33,56 @@ const theme=createTheme({
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Container>
-    <div className="App">
-      <Typography variant="h2">
-        Welcome to Material UI
-      </Typography>
+      <Container maxWidth="sm">
+        <div className="App">
+          <AppBar>
+            <Toolbar>
+              <IconButton>
+                <Menu color="secondary"></Menu>
+              </IconButton>
+              <Typography variant="h6">
+                MUI Styling
+              </Typography>
+              <Button color="inherit">
+                Login
+              </Button>
+            </Toolbar>
+          </AppBar>
+          <Typography variant="h2" style={{marginTop:60}}>
+            Welcome to Material UI
+          </Typography>
 
-      <Typography variant="subtitle1">
-        Learn how to use MUI
-      </Typography>
+          <Typography variant="subtitle1">
+            Learn how to use MUI
+          </Typography>
 
-      
-      <ButtonComponent />
-      <CheckBoxComponent />
-      <TextFieldComponent />
-      <StyledButtonComponent />
 
-    </div>
-    </Container>
+          <ButtonComponent />
+          <CheckBoxComponent />
+
+          <Grid container spacing={3} justifyContent='center'>
+            <Grid item xs={12} sm={6}>
+              <Paper style={{ width:'100%', height: 75,backgroundColor:'dodgerblue'}} />
+            </Grid>
+
+            <Grid item>
+              <Paper style={{ width: 50, height: 75 }} />
+            </Grid>
+
+            <Grid item>
+              <Paper style={{ width: 50, height: 75 }} />
+            </Grid>
+
+            <Grid item>
+              <Paper style={{ width: 50, height: 75 }} />
+            </Grid>
+          </Grid>
+
+          <TextFieldComponent />
+          <StyledButtonComponent />
+
+        </div>
+      </Container>
     </ThemeProvider>
   );
 }
